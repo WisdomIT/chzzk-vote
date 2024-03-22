1d44484085fc5345e2f3416885b9ce83a5a37b9f
HyunMyeong Lim <bs03166@naver.com>
2024-03-22 22:05:10 +0900

    Feat: 기본 스타일 정의

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,13 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
+import StyledComponentsRegistry from "@/components/StyledComponentsRegistry";
+import Layout from "@/components/Layout";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  
+  return <StyledComponentsRegistry>
+    <Layout>
+      <Component {...pageProps} />
+    </Layout>
+  </StyledComponentsRegistry>
 }
