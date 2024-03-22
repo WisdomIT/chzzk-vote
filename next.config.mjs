1384acb7a15c3e477f55b7f1b0d3ecc7c5be4240
HyunMyeong Lim <bs03166@naver.com>
2024-03-22 11:09:02 +0900

    Update: 패키지 설치

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,21 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  compiler: {
+    styledComponents: true
+  },
+  async rewrites() {
+    return [
+      {
+        source: "/api/proxy/chzzkBase/:path*",
+        destination: "https://api.chzzk.naver.com/:path*"
+      },
+      {
+        source: "/api/proxy/gameBase/:path*",
+        destination: "https://comm-api.game.naver.com/nng_main/:path*"
+      }
+    ]
+  }
 };
 
 export default nextConfig;
