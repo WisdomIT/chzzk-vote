774a88f82d37ce92def7c721b48b744666285eb6
HyunMyeong Lim <bs03166@naver.com>
2024-03-24 17:48:34 +0900

    Feat: Footer 링크 추가

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -252,7 +252,7 @@ const FooterLink = styled(Link)`
 const Footer = () => {
   return <FooterFrame>
     <FooterLeft>
-      <Copyright>© WisdomIT</Copyright>
+      <Copyright>© <FooterLink href="https://www.discord.com/users/901304044767834123" target="_blank">WisdomIT</FooterLink></Copyright>
       <Thirdparty>치지직 투표 추첨기는 <FooterLink href="https://chzzk.naver.com/" target="_blank">치지직</FooterLink>의 써드파티 사이트로, 치지직에서 운영하는 사이트가 아닙니다<br/>“치지직”은 NAVER Corp.의 등록 상표입니다</Thirdparty>
     </FooterLeft>
     <SpecialThanks>Special thanks to. <FooterLink href="https://chzzk.naver.com/ca1850b2eceb7f86146695fd9bb9cefc" target="_blank">빅헤드</FooterLink> <FooterLink href="https://chzzk.naver.com/219d8e65810a77d6e42c7df018d9632b" target="_blank">마뫄</FooterLink></SpecialThanks>
