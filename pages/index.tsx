5eeef16ffc46a85c6bd21843623fe59c9e7b0748
WisdomIT <bs03166@naver.com>
2024-11-19 03:36:59 +0900

    feat: 도네투표 추가
    - 메인 페이지 툴팁 추가
    - 도네 투표 항목 추가 (양아지님 요청)
    - 투표 시 항목 갯수가 화면을 초과할 때 스크롤바가 나타나도록 수정

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,6 @@
 import Head from "next/head";
 import styled from "styled-components";
-import { size, device, truncate } from '@/lib/style'
+import { size, device, truncate } from "@/lib/style";
 import Link from "next/link";
 
 const Frame = styled.div`
@@ -13,17 +13,18 @@ const Frame = styled.div`
   align-items: center;
   padding: 20px;
 
-  animation: appearUp .3s;
+  animation: appearUp 0.3s;
 
   @media ${device.mobile} {
     padding: 80px 20px;
     flex-direction: column;
     gap: 20px;
   }
-`
+`;
 
 const Btn = styled(Link)`
   display: flex;
+  position: relative;
   flex-direction: column;
   justify-content: center;
   align-items: center;
@@ -36,13 +37,12 @@ const Btn = styled(Link)`
   .dark & {
     border: 1px solid var(--color-stroke-01);
   }
-  
+
   .light & {
     border: 1px solid var(--color-stroke-light-01);
   }
 
   &:hover {
-
     .dark & {
       border: 3px solid var(--color-brand);
     }
@@ -50,11 +50,10 @@ const Btn = styled(Link)`
     .light & {
       border: 5px solid var(--color-brand-light);
     }
-
   }
 
-  &:hover i, &:hover p {
-
+  &:hover i,
+  &:hover p {
     .dark & {
       color: var(--color-brand);
     }
@@ -62,7 +61,10 @@ const Btn = styled(Link)`
     .light & {
       color: var(--color-brand-light);
     }
+  }
 
+  &:hover span {
+    animation: appearUp 0.3s forwards;
   }
 
   @media ${device.tablet} {
@@ -75,8 +77,7 @@ const Btn = styled(Link)`
     height: 100px;
     flex-direction: row;
   }
-
-`
+`;
 
 const BtnIcon = styled.i`
   font-size: 80px;
@@ -92,7 +93,7 @@ const BtnIcon = styled.i`
   @media ${device.mobile} {
     font-size: 40px;
   }
-`
+`;
 
 const BtnText = styled.p`
   font: 800 20px/1 var(--font-default);
@@ -100,7 +101,22 @@ const BtnText = styled.p`
   .dark & {
     color: var(--color-white);
   }
-`
+`;
+
+const BtnTooltip = styled.span`
+  display: block;
+  position: absolute;
+  bottom: -80px;
+  left: -4px;
+  right: -4px;
+  background-color: var(--color-brand);
+  color: var(--color-background-01);
+  padding: 10px;
+  text-align: center;
+  font: 600 16px/1.3 var(--font-default);
+  border-radius: 8px;
+  opacity: 0;
+`;
 
 export default function Home() {
   return (
@@ -115,16 +131,39 @@ export default function Home() {
         <Btn href="/viewer">
           <BtnIcon className="fa-sharp fa-light fa-users" />
           <BtnText>시청자 추첨</BtnText>
+          <BtnTooltip>
+            랜덤한 시청자를
+            <br />
+            추첨을 통해 뽑습니다
+          </BtnTooltip>
         </Btn>
         <Btn href="/vote">
           <BtnIcon className="fa-sharp fa-light fa-check-to-slot" />
           <BtnText>숫자 투표</BtnText>
+          <BtnTooltip>
+            항목을 정해두고
+            <br />
+            시청자들이 채팅으로 투표합니다
+          </BtnTooltip>
+        </Btn>
+        <Btn href="/donation">
+          <BtnIcon className="fa-sharp fa-light fa-cheese-swiss" />
+          <BtnText>도네 투표</BtnText>
+          <BtnTooltip>
+            항목을 정해두고
+            <br />
+            시청자들이 도네로 투표합니다
+          </BtnTooltip>
         </Btn>
         <Btn href="/roulette">
           <BtnIcon className="fa-sharp fa-light fa-slot-machine" />
           <BtnText>룰렛</BtnText>
+          <BtnTooltip>
+            항목을 정해두고
+            <br />그 중 하나를 룰렛으로 뽑습니다
+          </BtnTooltip>
         </Btn>
       </Frame>
     </>
-  )
+  );
 }
