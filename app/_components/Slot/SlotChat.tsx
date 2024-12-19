eca87cd7ed1602953e5e6106200bef023d197716
WisdomIT <bs03166@naver.com>
2024-12-19 14:13:49 +0900

    ✨ feat: SlotChat 컴포넌트 작성 - Slot을 활용해 list 내에서 랜덤으로 뷰어를 선정해 Chat 컴포넌트와 같은 기능을 하도록 작성된 컴포넌트 - memo로 상위 컴포넌트의 drawn 상태가 변경되더라도 리렌더링을 방지 - onEnd가 SlotChat의 렌더링 후 발현되므로 appear 애니메이션 중 결과가 스포일러되는 것을 방지

---
+++ app/_components/Slot/SlotChat.tsx
@@ -0,0 +1,83 @@
+"use client";
+
+import { ViewerType } from "@/lib/types";
+import { memo, useEffect, useState } from "react";
+import { useGlobalOptionStore } from "@/lib/zustand";
+import Slot from "./Slot";
+import { Container, ChatBox, Balloon, ChatBottom } from "./Chat.styled";
+import useVoice from "@/lib/useVoice";
+import useChzzkChat from "@/lib/useChzzkChat";
+import MainButton from "../Main/MainButton";
+
+interface SlotChatProps {
+  list: ViewerType[];
+  duration: number;
+  onEnd?: (viewer: ViewerType, index: number) => void;
+  onClose: () => void;
+}
+
+const SlotChat = memo(({ list, duration, onEnd, onClose }: SlotChatProps) => {
+  const { channel, voice } = useGlobalOptionStore();
+  const [viewer, setViewer] = useState<null | ViewerType>(null);
+  const [chat, setChat] = useState<JSX.Element[]>([]);
+
+  const handleSlotEnd = (viewer: ViewerType, index: number) => {
+    setViewer(viewer);
+    if (onEnd) {
+      onEnd(viewer, index);
+    }
+  };
+
+  function handleOnChat(
+    getViewer: ViewerType,
+    message: JSX.Element,
+    messageVoice: string
+  ) {
+    if (!viewer) return;
+
+    if (viewer.userIdHash !== getViewer.userIdHash) return;
+    setChat((prev) => [...prev, message]);
+    useVoice(voice, messageVoice);
+  }
+
+  useEffect(() => {
+    if (!viewer) return;
+
+    const client = useChzzkChat({
+      channelId: channel.channelId,
+      onChat: handleOnChat,
+    });
+
+    return () => {
+      client.disconnect();
+    };
+  }, [viewer]);
+
+  return (
+    <Container>
+      <Slot
+        list={list}
+        duration={duration}
+        onEnd={handleSlotEnd}
+        initialTarget={viewer}
+      />
+      {viewer ? (
+        <>
+          <ChatBox>
+            {chat.map((e, i) => (
+              <Balloon key={`chat_balloon_${i}`}>{e}</Balloon>
+            ))}
+            <ChatBottom id="chatBottom" />
+          </ChatBox>
+          <MainButton fill="primary" fillType="outlined" onClick={onClose}>
+            닫기
+          </MainButton>
+        </>
+      ) : null}
+    </Container>
+  );
+});
+
+SlotChat.displayName = "SlotChat";
+
+export default SlotChat;
