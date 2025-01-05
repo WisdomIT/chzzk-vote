f6eba4443aa41192407e9e86c736b0870203ec22
WisdomIT <bs03166@naver.com>
2025-01-06 06:59:17 +0900

    ✨ feat: 도네 투표 타이머 기능 적용

--- app/(main)/donation/_views/Running.tsx
+++ app/(main)/donation/_views/Running.tsx
@@ -29,15 +29,17 @@ import {
 import { DoneConfigType } from "../page";
 import { webhook } from "../../_api/webhook";
 import ChzzkError from "@/app/_components/Viewer/ChzzkError";
+import Timer from "@/app/_components/Vote/Timer";
 
 export default function Running({
   doneConfig,
   vote,
   setVote,
-  onStop,
   drawn,
-  time,
   setDrawn,
+  time,
+  timer,
+  onStop,
 }: {
   doneConfig: DoneConfigType;
   vote: VoteType[];
@@ -45,6 +47,7 @@ export default function Running({
   drawn: ViewerType[];
   setDrawn: (viewer: ViewerType) => void;
   time: TimeType;
+  timer: Date | null;
   onStop: () => void;
 }) {
   const { channel } = useGlobalOptionStore();
@@ -268,6 +271,7 @@ export default function Running({
           <MainButton onClick={onStop}>투표 종료</MainButton>
         </Bottom>
       </ContainerCenter>
+      {timer ? <Timer end={timer} onStop={onStop} /> : null}
     </Container>
   );
 }
