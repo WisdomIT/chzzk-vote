b0c7bc410322f727bb31339a662dfc75af700839
WisdomIT <bs03166@naver.com>
2025-01-25 20:18:08 +0900

    💄 style: zoom 기능을 위한 스타일 추가

--- app/(main)/config/_components/index.styled.tsx
+++ app/(main)/config/_components/index.styled.tsx
@@ -1,7 +1,7 @@
 import styled from "styled-components";
 
 export const Container = styled.main`
-  width: 100vw;
+  width: 100%;
   height: 100%;
   display: flex;
   flex-direction: column;
@@ -22,6 +22,7 @@ export const Btns = styled.div`
   gap: 20px;
   width: 100%;
   justify-content: center;
+  flex-shrink: 0;
 
   ${({ theme }) => theme.device.mobile} {
     flex-direction: column;
